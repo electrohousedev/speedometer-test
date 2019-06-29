bc6b61e4b5315ba7da681eb01d116e7d58bf502f
andryblack <blackicebox@gmail.com>
2019-06-30 01:24:40 +0300

    CircleCI configuration

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -16,13 +16,31 @@ git submodule update --init --recursive || exit 1
 BOOTSTRAPPREMAKE=sandbox/bin/$PLATFORM/premake5$EXE
 #rm -f $BOOTSTRAPPREMAKE
 if [ ! -f $BOOTSTRAPPREMAKE ]; then
+    echo "bootstrap premake5 at $PLATFORM"
+    
+    
+    find ./sandbox/utils/premake5 -name "obj" -type d -exec rm -rf {} +
+    find ./sandbox/utils/premake5 -name "bin" -type d -exec rm -rf {} +
+    rm -rf build/bootstrap
+
     mkdir -p sandbox/bin/$PLATFORM
     cd sandbox/utils/premake5
     make -f Bootstrap.mak $PLATFORM ||  echo "make failed"
     ./build/bootstrap/premake_bootstrap --to=build/bootstrap --no-zlib=true --no-curl=true gmake 
-    make -C build/bootstrap 
+    make -C build/bootstrap clean
+    make -C build/bootstrap V=1
     cd ../../..
     cp sandbox/utils/premake5/bin/release/premake5 $BOOTSTRAPPREMAKE
+
+    rm -rf sandbox/utils/premake5/obj
+    rm -rf sandbox/utils/premake5/bin
+
+    find ./sandbox/utils/premake5 -name "obj" -type d -exec rm -rf {} +
+    find ./sandbox/utils/premake5 -name "bin" -type d -exec rm -rf {} +
+
+    rm -rf build/bootstrap
+
+    echo "premake5 ready"
 fi
 
 cd sandbox/utils
