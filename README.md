b96753e5769097a87403b044b5b21d9c20f56ed8
andryblack <blackicebox@gmail.com>
2019-07-02 06:34:46 +0300

    setup build on windows

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 [![CircleCI](https://circleci.com/gh/electrohousedev/speedometer-test/tree/master.svg?style=svg)](https://circleci.com/gh/electrohousedev/speedometer-test/tree/master)
 
-## Build 
+## Build on Linux or OSX
 
 Initialize and build dependencies.
 Need only once.
@@ -28,6 +28,72 @@ Need after add files and change settings.
 Build android apk.
 
 ```
+cd projects/android
+./gradlew assembleRelease
+```
+
+## Build on Windows 
+
+Install android SDK (Command line tools only)
+
+Download and unzip sdk-tools-windows-.zip from https://developer.android.com/studio#downloads 
+
+Eg C:\sdk\android-sdk
+
+Download and unzip android NDK (r19C) from https://developer.android.com/ndk/downloads/older_releases.html
+
+Eg C:\sdk\android-ndk-r19c
+
+Install JDK http://jdk.java.net/12/ 
+
+Eg C:\sdk\jdk-12.0.1
+
+Build host tools with gnu toolchain.
+
+
+Install MSYS2 (https://www.msys2.org) or Cygwin (https://www.cygwin.com)
+
+Install development packages.
+
+MSYS2:
+
+use mingw64 shell: open C:\msys64\mingw64.exe
+
+```
+pacman -S git mingw-w64-x86_64-toolchain bash make curl unzip
+```
+
+clone this project
+
+```
+git clone https://github.com/electrohousedev/speedometer-test.git
+cd speedometer-test
+```
+Download submodules and build host tools
+```
+./scripts/bootstrap.sh
+```
+
+Create file `config.sh` at root of repo with MSYS paths to SDK and NDK, eg:
+```
+ANDROIDSDK=/c/sdk/android-sdk
+ANDROIDNDK=/c/sdk/android-ndk-r19c
+```
+
+Download android SDK components:
+```
+/c/sdk/android-sdk/tools/bin/sdkmanager.bat "platforms;android-28"
+/c/sdk/android-sdk/tools/bin/sdkmanager.bat "build-tools;28.0.3"
+```
+
+Generate projects
+``` 
+./scripts/make_project.sh
+```
+
+Build apk.
+``` 
+export JAVA_HOME=/c/sdk/jdk-12.0.1
 cd projects/android
 ./gradlew assembleRelease
 ```
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,28 +2,75 @@
 
 PLATFORM=`uname -s`
 
-if [ "$PLATFORM" = "Darwin" ]; then
-    xcode-select --install
-    PLATFORM=osx
-fi
-if [ "$PLATFORM" = "Linux" ]; then
-    PLATFORM=linux
-fi
+case "$PLATFORM" in
+
+   Darwin)
+     xcode-select --install
+     PLATFORM=osx
+     ;;
+
+   Linux)
+     PLATFORM=linux
+     ;;
+
+   CYGWIN*|MSYS*|MINGW32*)
+     echo "please use mingw64 shell";
+     exit 1;
+     ;;
+
+   MINGW64*)
+     PLATFORM=windows
+     EXE=".exe"
+     ;;
+
+   # Add here more strings to compare
+   # See correspondence table at the bottom of this answer
+
+   *)
+     echo 'other OS' 
+     ;;
+esac
+
 
 git submodule init || exit 1
 git submodule update --init --recursive || exit 1
 
-BOOTSTRAPPREMAKE=sandbox/bin/$PLATFORM/premake5$EXE
+ROOT=`pwd`
+BOOTSTRAPPREMAKE=$ROOT/sandbox/bin/$PLATFORM/premake5$EXE
+
+if [ "$PLATFORM" == "windows" ]; then
+    # build broken, download
+    if [ ! -f $BOOTSTRAPPREMAKE ]; then
+        rm premake5.exe
+        rm premake5.zip
+        curl -L "https://github.com/premake/premake-core/releases/download/v5.0.0-alpha14/premake-5.0.0-alpha14-windows.zip" -o premake.zip 
+        unzip premake.zip
+        mv premake5.exe $BOOTSTRAPPREMAKE
+        rm premake.zip
+    fi
+  
+    cd sandbox/utils
+
+    echo "rebuild assetsbuilder"
+    cd assetsbuilder
+    ../../bin/$PLATFORM/premake5.exe --scripts=../../premake gmake --to=. || exit 1
+    make config=release -j2 || exit 1
+    cp bin/release/*.exe $ROOT/sandbox/bin/$PLATFORM/
+    cd ..
+
+    exit 0
+fi
+
 #rm -f $BOOTSTRAPPREMAKE
 if [ ! -f $BOOTSTRAPPREMAKE ]; then
     echo "bootstrap premake5 at $PLATFORM"
     
-    
+    mkdir -p sandbox/bin/$PLATFORM
+       
     find ./sandbox/utils/premake5 -name "obj" -type d -exec rm -rf {} +
     find ./sandbox/utils/premake5 -name "bin" -type d -exec rm -rf {} +
     rm -rf build/bootstrap
 
-    mkdir -p sandbox/bin/$PLATFORM
     cd sandbox/utils/premake5
     make -f Bootstrap.mak $PLATFORM ||  echo "make failed"
     ./build/bootstrap/premake_bootstrap --to=build/bootstrap --no-zlib=true --no-curl=true gmake 
@@ -39,6 +86,8 @@ if [ ! -f $BOOTSTRAPPREMAKE ]; then
     find ./sandbox/utils/premake5 -name "bin" -type d -exec rm -rf {} +
 
     rm -rf build/bootstrap
+
+    
 
     echo "premake5 ready"
 fi
